e45c77cfaf17c60510b17f097ee64bfa2e5690cc
giridhar <132940466+giri69@users.noreply.github.com>
2024-04-14 19:59:44 +0530

    Add files via upload

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -39,106 +39,10 @@ export const Products = () => {
 
               <div class="tab-pane fade" id="ring" role="tabpanel" aria-labelledby="ring-tab">
                 <div class="row">
-                  <Item img="./images/product-img/r-1.jpg"/>
-                  <div class="col-md-3 col-sm-6 mb-3">
-                    <div class="item-product">
-                      <a href="#" class="product-thumb-link">
-                        <img decoding="async" src="./images/product-img/r-2.jpg" class="img-fluid"/>
-                      </a>
-                    </div>
-                    <div class="product-info">
-                      <div class="d-flex justify-content-between py-3">
-                        <a href="#" class="cat-parent">Women</a>
-                        <a href="#" class="wishlist">
-                          <i class="fa fa-heart"></i>
-                        </a>
-                      </div>
-                      <h4 class="product-title">
-                        <a href="#">Titanium ring in ged palladiumfinish metal</a>
-                      </h4>
-                      <div class="product-price">
-                        <ins><span class="money text-white">$300.00</span></ins>
-                      </div>
-                      <div class="d-flex align-items-center justify-content-between py-1">
-                        <div class="rating">
-                          <i class="far fa-star"></i>
-                          <i class="far fa-star"></i>
-                          <i class="far fa-star"></i>
-                          <i class="far fa-star"></i>
-                          <i class="far fa-star"></i>
-                        </div>
-                        <div class="basket">
-                          <a href="#"><i class="fas fa-shopping-basket"></i></a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-md-3 col-sm-6 mb-3">
-                    <div class="item-product">
-                      <a href="#" class="product-thumb-link">
-                        <img decoding="async" src="./images/product-img/r-3.jpg" class="img-fluid"/>
-                      </a>
-                    </div>
-                    <div class="product-info">
-                      <div class="d-flex justify-content-between py-3">
-                        <a href="#" class="cat-parent">Women</a>
-                        <a href="#" class="wishlist">
-                          <i class="fa fa-heart"></i>
-                        </a>
-                      </div>
-                      <h4 class="product-title">
-                        <a href="#">Titanium ring in ged palladiumfinish metal</a>
-                      </h4>
-                      <div class="product-price">
-                        <ins><span class="money text-white">$300.00</span></ins>
-                      </div>
-                      <div class="d-flex align-items-center justify-content-between py-1">
-                        <div class="rating">
-                          <i class="far fa-star"></i>
-                          <i class="far fa-star"></i>
-                          <i class="far fa-star"></i>
-                          <i class="far fa-star"></i>
-                          <i class="far fa-star"></i>
-                        </div>
-                        <div class="basket">
-                          <a href="#"><i class="fas fa-shopping-basket"></i></a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-md-3 col-sm-6 mb-3">
-                    <div class="item-product">
-                      <a href="#" class="product-thumb-link">
-                        <img decoding="async" src="./images/product-img/r-4.jpg" class="img-fluid"/>
-                      </a>
-                    </div>
-                    <div class="product-info">
-                      <div class="d-flex justify-content-between py-3">
-                        <a href="#" class="cat-parent">Women</a>
-                        <a href="#" class="wishlist">
-                          <i class="fa fa-heart"></i>
-                        </a>
-                      </div>
-                      <h4 class="product-title">
-                        <a href="#">Titanium ring in ged palladiumfinish metal</a>
-                      </h4>
-                      <div class="product-price">
-                        <ins><span class="money text-white">$300.00</span></ins>
-                      </div>
-                      <div class="d-flex align-items-center justify-content-between py-1">
-                        <div class="rating">
-                          <i class="far fa-star"></i>
-                          <i class="far fa-star"></i>
-                          <i class="far fa-star"></i>
-                          <i class="far fa-star"></i>
-                          <i class="far fa-star"></i>
-                        </div>
-                        <div class="basket">
-                          <a href="#"><i class="fas fa-shopping-basket"></i></a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                <Item img="./images/product-img/r-1.jpg" />
+                 <Item img="./images/product-img/r-2.jpg"/>
+                 <Item img="./images/product-img/r-3.jpg"/>
+                 <Item img="./images/product-img/r-4.jpg"/>
                 </div>
               </div>
 
@@ -246,7 +150,7 @@ export const Products = () => {
                   <div class="col-md-3 col-sm-6 mb-3">
                     <div class="item-product">
                       <a href="#" class="product-thumb-link">
-                        <img decoding="async" src="./images/product-img/e-4.jpg" class="img-fluid"/>
+                        <img decoding="async" src="./images/product-img/b-4.jpg" class="img-fluid"/>
                       </a>
                     </div>
                     <div class="product-info">
